b016a4467d27d4d71ba05f72be2a316affbd96bd
Jamal Portericker <jportericker@yahoo.com>
2023-07-17 14:12:39 -0400

    models/users/shopping

---
+++ server/models/shopping_cart.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+class ShoppingCart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    items = db.relationship('Item', backref='shopping_cart')
+
