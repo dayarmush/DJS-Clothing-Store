f8fd1507a292e8df639957c5317f532adf6c1c83
Dovid Yarmush <yarmushda@gmail.com>
2023-07-19 10:50:19 -0400

    app fetch and card component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import ItemCard from './components/ItemCard';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//   const [items, setItems] = useState()
+
+//   useEffect(() => {
+//     fetch('http://127.0.0.1:5555/items')
+//     .then(r => r.json())
+//     .then(data => setItems(data))
+//   }, [])
+
+//   const itemsMap = items.map(item => {
+//     <ItemCard item={item}/>
+//   })
+
+
+//   return (
+//     {itemsMap}
+//   );
 }
 
 export default App;
